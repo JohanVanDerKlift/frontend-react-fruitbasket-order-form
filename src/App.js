1b333286ab097966c39932dd4775a7f936d4aa08
Klippie <info@johanvanderklift.nl>
2022-11-22 17:20:04 +0100

    Finished assignment 1, minimal styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import FruitCounter from "./components/FruitCounter";
 import kiwi from "./assets/kiwi.png"
@@ -7,16 +7,78 @@ import banaan from "./assets/banaan.png"
 import appel from "./assets/appel.png"
 
 function App() {
+  const [ state, setState ] = useState({
+    strawberry: 0,
+    banana: 0,
+    apple: 0,
+    kiwi: 0,
+  });
+
+  function addOne(e) {
+    const changedItem = e.target.name;
+    console.log(changedItem);
+    setState({
+      ...state,
+      [changedItem]: state[changedItem].valueOf() + 1,
+    });
+  }
+
+  function subtractOne(e) {
+    const changedItem = e.target.name;
+    if (state[changedItem].valueOf() > 0) {
+      setState({
+        ...state,
+        [changedItem]: state[changedItem].valueOf() - 1,
+      });
+    }
+  }
+
   return (
     <>
       <h1>Fruitmand bezorgservice</h1>
       <ul className="fruit-list">
-        <FruitCounter image={aardbei} item="Aardbei"/>
-        <FruitCounter image={banaan} item="banaan"/>
-        <FruitCounter image={appel} item="appel"/>
-        <FruitCounter image={kiwi} item="kiwi"/>
+        <FruitCounter
+          name="strawberry"
+          image={aardbei}
+          item="Aardbei"
+          addOne={addOne}
+          subtractOne={subtractOne}
+          value={state.strawberry}
+        />
+        <FruitCounter
+          name="banana"
+          image={banaan}
+          item="banaan"
+          addOne={addOne}
+          subtractOne={subtractOne}
+          value={state.banana}
+        />
+        <FruitCounter
+          name="apple"
+          image={appel}
+          item="appel"
+          addOne={addOne}
+          subtractOne={subtractOne}
+          value={state.apple}
+        />
+        <FruitCounter
+          name="kiwi"
+          image={kiwi}
+          item="kiwi"
+          addOne={addOne}
+          subtractOne={subtractOne}
+          value={state.kiwi}
+        />
+        <button type="button" onClick={() => {
+          setState({
+            strawberry: 0,
+            banana: 0,
+            apple: 0,
+            kiwi: 0,
+          });
+          FruitCounter(state);
+        }}>Reset</button>
       </ul>
-
     </>
   );
 }
--- src/components/FruitCounter.js
+++ src/components/FruitCounter.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-function FruitCounter({image, item}) {
+function FruitCounter({name, image, item, addOne, subtractOne ,value}) {
+
   return (
     <>
       <label htmlFor="counter"><img src={image} alt="" className="fruit-image"/>{item}
-        <button>-</button>
-        <input type="text" id="counter"/>
-        <button>+</button>
+        <button
+          type="button"
+          name={name}
+          onClick={subtractOne}
+          disabled={value <= 0}
+        >-</button>
+        <input
+          name={name}
+          type="number"
+          id="counter"
+          value={value}
+        />
+        <button
+          type="button"
+          name={name}
+          onClick={addOne}
+        >+</button>
       </label>
     </>
   );
